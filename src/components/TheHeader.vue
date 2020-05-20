842f1ab5736e7a314d63c9bc548988f97ed2a534
ThanhCuD <thanhduc552018@gmail.com>
2020-05-20 14:20:15 +0700

    change

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,6 @@
 <template>
   <nav class="navbar navbar-light">
     <div class="container">
-      <router-link class="navbar-brand" :to="{ name: 'home' }">conduit</router-link>
       <ul class="nav navbar-nav pull-xs-right">
         <li class="nav-item">
           <router-link class="nav-link" active-class="active" exact :to="{ name: 'home' }">Home</router-link>
